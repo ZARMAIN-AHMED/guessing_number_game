712931bc6bf1d88b7295e222746af405345b5e07
ZARMAIN-AHMED <nazzshagufta604@gmail.com>
2024-04-01 22:03:33 +0500

    CLI GUESSING NUMBER GAME

---
+++ game.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env  node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 1 + 10);
+const answer = await inquirer.prompt([{
+        name: "userguessnumber",
+        type: "number",
+        message: "please guess a number:",
+    },
+]);
+if (answer.userguessnumber === randomNumber) {
+    console.log("congragulation! you guess right number");
+}
+else {
+    console.log("you guessed wrong number");
+}
